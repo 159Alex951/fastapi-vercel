e9fe5abc4474bb54d2b9cf00af3253e8cb5225a0
159Alex951 <162427178+159Alex951@users.noreply.github.com>
2024-12-18 11:02:00 +0100

    more clean up

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <h1>
+        Dies ist meine Projektarbeit: Bitte Bankverbindung angeben damit ich dir
+        das Geld für eine genügende Note überweisen kann.
+      </h1>
+      <h1>_____________________________</h1>
+      <h2>Besten Dank</h2>
+    </>
+  );
+}
