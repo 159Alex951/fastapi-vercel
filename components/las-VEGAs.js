0d0bae7f2130ff2c2e281b8b963e555dc055e96e
159Alex951 <162427178+159Alex951@users.noreply.github.com>
2025-01-14 14:48:34 +0100

    Grafik schnell reingepackt
    Jetzt kurz ein paar 😻 📹 schauen mit Michi 👨

---
+++ components/las-VEGAs.js
@@ -0,0 +1,41 @@
+// components/WeatherVegaLiteChart.js
+import React, { useEffect, useRef } from "react";
+import vegaEmbed from "vega-embed";
+
+const WeatherVegaLiteChart = ({ data }) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const chartData = data.map((item) => ({
+        date: new Date(item.Datum).toLocaleDateString(),
+        temperature: item.T,
+        location: item.Standortname,
+      }));
+
+      const spec = {
+        $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+        description: "Temperature over time",
+        data: {
+          values: chartData,
+        },
+        mark: "line",
+        encoding: {
+          x: { field: "date", type: "temporal", title: "Datum" },
+          y: {
+            field: "temperature",
+            type: "quantitative",
+            title: "Temperatur (°C)",
+          },
+          color: { field: "location", type: "nominal", title: "Standort" },
+        },
+      };
+
+      vegaEmbed(chartRef.current, spec);
+    }
+  }, [data]);
+
+  return <div ref={chartRef}></div>;
+};
+
+export default WeatherVegaLiteChart;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,35 @@
-import WeatherApp from "/components/weatherapp";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import WeatherVegaLiteChart from "/components/las-VEGAs";
+import WeatherApp from "/components/WeatherApp";
 
-export default function Home() {
+const Home = () => {
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("api/py/meteodaten")
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error("Fehler beim Laden der Wetterdaten:", error);
+      });
+  }, []);
+
   return (
     <div>
-      <WeatherApp />
+      <h1>Wetterdaten Visualisierung mit Vega-Lite</h1>
+      {weatherData.length > 0 ? (
+        <WeatherVegaLiteChart data={weatherData} />
+      ) : (
+        <p>Lade Wetterdaten...</p>
+      )}
+      <div>
+        <WeatherApp />
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
